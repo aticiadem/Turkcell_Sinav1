e6dc84c6c44ba2acf7c54e5f958c8911e7bf81b6
Adem <adematicis41@gmail.com>
2022-03-21 13:58:11 +0300

    Complete Login Screen logic

--- app/src/main/java/com/turkcell/sinav1/MainActivity.kt
+++ app/src/main/java/com/turkcell/sinav1/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.turkcell.sinav1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.turkcell.sinav1.databinding.ActivityMainBinding
+import com.turkcell.sinav1.util.Constants.DEFAULT_PASSWORD
+import com.turkcell.sinav1.util.Constants.DEFAULT_TEL_NO
+import com.turkcell.sinav1.util.Constants.INTENT_USER_DID_LOG_IN
+import com.turkcell.sinav1.util.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var intentToProductScreen: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intentToProductScreen = Intent(this, ProductsActivity::class.java)
+
+        binding.buttonContinue.setOnClickListener {
+            intent.putExtra(INTENT_USER_DID_LOG_IN, false)
+            startActivity(intentToProductScreen)
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            val telNo = binding.editTextTelNo.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            checkUserInputState(telNo, password)
+        }
+
     }
+
+    private fun checkUserInputState(telNo: String, password: String) {
+        if (telNo.isNotEmpty() && password.isNotEmpty()) {
+            if (telNo == DEFAULT_TEL_NO && password == DEFAULT_PASSWORD) {
+                intent.putExtra(INTENT_USER_DID_LOG_IN, true)
+                startActivity(intentToProductScreen)
+                finish()
+            } else {
+                this.toast(getString(R.string.input_error))
+            }
+        } else {
+            this.toast(getString(R.string.fill_in_the_blanks))
+        }
+    }
+
 }
---
+++ app/src/main/java/com/turkcell/sinav1/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.turkcell.sinav1.util
+
+object Constants {
+
+    const val INTENT_USER_DID_LOG_IN = "didLogIn"
+
+    const val DEFAULT_TEL_NO = "01111111111"
+    const val DEFAULT_PASSWORD = "#e&it1m"
+
+}
